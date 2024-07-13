ab370c26d31f7bb1eb808cafc9b1a97db8983cdb
IgorTecnologia <iguigu2019@gmail.com>
2024-07-13 00:13:23 -0300

    feat: ApiComercioApplicationTests

---
+++ api-comercio/src/test/java/br/com/empresa/api_comercio/ApiComercioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.empresa.api_comercio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiComercioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
