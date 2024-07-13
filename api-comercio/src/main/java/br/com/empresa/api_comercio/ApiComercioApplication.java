a71db194776773a51e9531cc592f0ca05e4d3769
IgorTecnologia <iguigu2019@gmail.com>
2024-07-12 23:57:16 -0300

    feat: ApiComercioApplication

---
+++ api-comercio/src/main/java/br/com/empresa/api_comercio/ApiComercioApplication.java
@@ -0,0 +1,13 @@
+package br.com.empresa.api_comercio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiComercioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiComercioApplication.class, args);
+	}
+
+}
