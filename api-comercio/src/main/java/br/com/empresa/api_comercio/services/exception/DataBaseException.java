06f501a8b9480ab94b64aaf2c04443f7e2cb04a1
IgorTecnologia <iguigu2019@gmail.com>
2024-07-13 00:23:47 -0300

    feat: Data Base Exception

---
+++ api-comercio/src/main/java/br/com/empresa/api_comercio/services/exception/DataBaseException.java
@@ -0,0 +1,9 @@
+package br.com.empresa.api_comercio.services.exception;
+
+public class DataBaseException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+
+	public DataBaseException(String msg) {
+		super(msg);
+	}
+}
