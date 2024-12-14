6dfe2186e18776d0c35e8bd1833d16a380fabc20
IgorTecnologia <iguigu2019@gmail.com>
2024-12-14 19:59:19 -0300

    Role Repository

--- api-comercio/src/main/java/br/com/empresa/api_comercio/repositories/RoleRepository.java
+++ api-comercio/src/main/java/br/com/empresa/api_comercio/repositories/RoleRepository.java
@@ -1,15 +1,14 @@
 package br.com.empresa.api_comercio.repositories;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import br.com.empresa.api_comercio.entities.Role;
 
-@Repository
-public interface RoleRepository extends JpaRepository<Role, Long>{
+public interface RoleRepository extends JpaRepository<Role, UUID>{
 
 	public List<Role> findAllByAuthorityContainingIgnoreCase(@Param("authority") String authority);
 }
