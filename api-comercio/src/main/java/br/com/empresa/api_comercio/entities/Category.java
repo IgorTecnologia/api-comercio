479a17645d74121b4d3c38ba9b837a25161b8ff6
IgorTecnologia <iguigu2019@gmail.com>
2024-12-14 19:54:43 -0300

    Category Entity

--- api-comercio/src/main/java/br/com/empresa/api_comercio/entities/Category.java
+++ api-comercio/src/main/java/br/com/empresa/api_comercio/entities/Category.java
@@ -2,54 +2,39 @@ package br.com.empresa.api_comercio.entities;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import lombok.Data;
 
+@Data
 @Entity
 @Table(name = "tb_category")
 public class Category {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private UUID id;
+
+	@Column(nullable = false)
 	private String name;
 	
-	@ManyToMany(mappedBy = "categories")
+	@ManyToMany(mappedBy = "categories", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
 	private List<Product> products = new ArrayList<>();
 	
 	public Category() {
 	}
 
-	public Category(Long id, String name) {
+	public Category(UUID id, String name) {
 		this.id = id;
 		this.name = name;
 	}
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public List<Product> getProducts() {
-		return products;
-	}
-	
-	
-	
 }
