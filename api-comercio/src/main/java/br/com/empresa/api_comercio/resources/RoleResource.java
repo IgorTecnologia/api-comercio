386ab06f60d62e8db08520f719005d13e7af419f
IgorTecnologia <iguigu2019@gmail.com>
2024-12-14 20:00:01 -0300

    Role Resource

--- api-comercio/src/main/java/br/com/empresa/api_comercio/resources/RoleResource.java
+++ api-comercio/src/main/java/br/com/empresa/api_comercio/resources/RoleResource.java
@@ -1,7 +1,9 @@
 package br.com.empresa.api_comercio.resources;
 
 import java.util.List;
+import java.util.UUID;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -16,14 +18,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.empresa.api_comercio.dto.RoleDTO;
-import br.com.empresa.api_comercio.services.RoleService;
+import br.com.empresa.api_comercio.services.impl.RoleServiceImpl;
 
 @RestController
 @RequestMapping(value = "/roles")
 public class RoleResource {
 
 	@Autowired
-	private RoleService service;
+	private RoleServiceImpl service;
 	
 	@GetMapping
 	public ResponseEntity<Page<RoleDTO>> findAllPaged(Pageable pageable){
@@ -40,30 +42,30 @@ public class RoleResource {
 	}
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<RoleDTO> findById(@PathVariable Long id){
+	public ResponseEntity<RoleDTO> findById(@PathVariable UUID id){
 		
 		RoleDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
 	}
 	
 	@PostMapping
-	public ResponseEntity<RoleDTO> insert(@RequestBody RoleDTO dto){
+	public ResponseEntity<RoleDTO> insert(@RequestBody @Valid RoleDTO dto){
 		
 		dto = service.insert(dto);
 		return ResponseEntity.ok().body(dto);
 	}
 	
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<RoleDTO> update(@PathVariable Long id, @RequestBody RoleDTO dto){
+	public ResponseEntity<RoleDTO> update(@PathVariable UUID id, @RequestBody @Valid RoleDTO dto){
 		
 		dto = service.update(id, dto);
 		return ResponseEntity.ok().body(dto);
 	}
 	
 	@DeleteMapping(value = "/{id}")
-	public ResponseEntity<Void> deleteById(@PathVariable Long id){
+	public ResponseEntity<Object> deleteById(@PathVariable UUID id){
 		
 		service.deleteById(id);
-		return ResponseEntity.noContent().build();
+		return ResponseEntity.ok().body("Role deleted successfully.");
 	}
 }
