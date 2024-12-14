a47bb95ced9a99e522549f7fcfb3b91daac34ebf
IgorTecnologia <iguigu2019@gmail.com>
2024-12-14 19:58:50 -0300

    Role Entity

--- api-comercio/src/main/java/br/com/empresa/api_comercio/entities/Role.java
+++ api-comercio/src/main/java/br/com/empresa/api_comercio/entities/Role.java
@@ -1,44 +1,33 @@
 package br.com.empresa.api_comercio.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
 
+import java.util.UUID;
+
+@Data
 @Entity
 @Table(name = "tb_role")
 public class Role {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private UUID id;
+
+	@Column(nullable = false)
 	private String authority;
 	
 	public Role() {
 	}
 
-	public Role(Long id, String authority) {
+	public Role(UUID id, String authority) {
 		this.id = id;
 		this.authority = authority;
 	}
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getAuthority() {
-		return authority;
-	}
-
-	public void setAuthority(String authority) {
-		this.authority = authority;
-	}
-	
-	
-	
 }
