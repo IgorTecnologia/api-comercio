3ca380b088d2e13a53bac2d2cba1f6daff838f49
IgorTecnologia <iguigu2019@gmail.com>
2024-12-19 06:06:02 -0300

    Category Service Tests

--- api-comercio/src/test/java/br/com/empresa/api_comercio/services/CategoryServiceTests.java
+++ api-comercio/src/test/java/br/com/empresa/api_comercio/services/CategoryServiceTests.java
@@ -2,7 +2,6 @@ package br.com.empresa.api_comercio.services;
 
 import br.com.empresa.api_comercio.dto.*;
 import br.com.empresa.api_comercio.entities.Category;
-import br.com.empresa.api_comercio.entities.User;
 import br.com.empresa.api_comercio.repositories.*;
 import br.com.empresa.api_comercio.services.exception.ResourceNotFoundException;
 import br.com.empresa.api_comercio.services.impl.CategoryServiceImpl;
@@ -10,9 +9,11 @@ import br.com.empresa.api_comercio.tests.*;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.boot.test.context.*;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.*;
 import org.springframework.transaction.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -20,25 +21,12 @@ import java.util.UUID;
 @SpringBootTest
 public class CategoryServiceTests {
 
-    private UUID existingId;
-    private UUID nonExistingId;
-    private Long countTotalElements;
-
     @Autowired
     private CategoryServiceImpl service;
 
     @Autowired
     private CategoryRepository repository;
 
-    @BeforeEach
-    void setUp() throws Exception{
-
-        Optional<Category> obj = repository.findAll().stream().findFirst();;
-        existingId = obj.orElseThrow(() -> new ResourceNotFoundException("Id not found: " + obj.get().getId())).getId();
-
-        nonExistingId = UUID.randomUUID();
-        countTotalElements = 3L;
-    }
 
     @Test
     public void findAllPagedShouldReturnAllCategories(){
@@ -51,7 +39,32 @@ public class CategoryServiceTests {
     }
 
     @Test
+    public void queryMethodShouldReturnAllCategoryFilteredByName(){
+
+        String name = "Doces";
+
+        List<CategoryDTO> list = service.queryMethod(name);
+
+        Assertions.assertFalse(list.isEmpty());
+        Assertions.assertNotNull(list);
+    }
+
+    @Test
+    public void queryMethodShouldThrowResourceNotFoundExceptionWhenNameNonExisting(){
+
+        String name = "Salgados";
+
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+           List<CategoryDTO> list = service.queryMethod(name);
+           throw new ResourceNotFoundException("Name not found: " + name);
+        });
+    }
+
+    @Test
     public void findByIdShouldReturnObjectWhenIdExisting(){
+
+        Optional<Category> obj = repository.findAll().stream().findFirst();;
+        UUID existingId = obj.orElseThrow(() -> new ResourceNotFoundException("Id not found: " + obj.get().getId())).getId();
 
         CategoryDTO dto = service.findById(existingId);
 
@@ -59,22 +72,72 @@ public class CategoryServiceTests {
     }
 
     @Test
+    public void findByIdShouldThrowResourceNotFoundExceptionWhenIdNonExisting(){
+
+        UUID id = UUID.randomUUID();
+
+        Assertions.assertThrows(ResourceNotFoundException.class,  () -> {
+            Optional<Category> obj = repository.findById(id);
+            throw new ResourceNotFoundException("Id not found: " + id);
+        });
+    }
+
+    @Test
     public void insertShouldSaveObjectWhenCorrectStructure(){
 
-        CategoryDTO dto = Factory.createdCategoryDTO();
+        CategoryDTO dto = Factory.createdCategoryDto();
 
         dto = service.insert(dto);
 
-        Assertions.assertEquals(countTotalElements +1, repository.count());
+        Assertions.assertEquals(4, repository.count());
     }
 
     @Test
     public void updateShouldSaveObjectWhenIdExisting(){
 
-        CategoryDTO dto = Factory.createdCategoryDTO();
+        Optional<Category> obj = repository.findAll().stream().findFirst();;
+        UUID existingId = obj.orElseThrow(() -> new ResourceNotFoundException("Id not found: " + obj.get().getId())).getId();
+
+        CategoryDTO dto = Factory.createdCategoryDtoToUpdate();
 
         dto = service.update(existingId, dto);
 
-        Assertions.assertEquals(countTotalElements, repository.count());
+        Assertions.assertNotNull(dto);
+        Assertions.assertEquals(3, repository.count());
+    }
+
+    @Test
+    public void updateShouldThrowResourceNotFoundExceptionWhenIdNonExisting(){
+
+        UUID id = UUID.randomUUID();
+
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+            CategoryDTO dto = Factory.createdCategoryDtoToUpdate();
+            dto = service.update(id, dto);
+            throw new ResourceNotFoundException("Id not found: " + id);
+        });
+    }
+
+    @Test
+    public void deleteByIdShouldThrowDataIntegrityViolationExceptionWhenIdIsAssociated(){
+
+        Optional<Category> obj = repository.findAll().stream().findFirst();;
+        UUID existingId = obj.orElseThrow(() -> new ResourceNotFoundException("Id not found: " + obj.get().getId())).getId();
+
+        Assertions.assertThrows(DataIntegrityViolationException.class, () -> {
+            service.deleteById(existingId);
+            throw new DataIntegrityViolationException("Data Integrity Violation Exception.");
+        });
+    }
+
+    @Test
+    public void deleteByIdShouldThrowResourceNotFoundExceptionWhenIdNonExisting(){
+
+        UUID id = UUID.randomUUID();
+
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+            service.deleteById(id);
+            throw new ResourceNotFoundException("Id not found: " + id);
+        });
     }
 }
