209e3b6940ea37fa060ba02a6f6d51ddd58ee423
IgorTecnologia <iguigu2019@gmail.com>
2024-12-19 06:01:50 -0300

    User Repository

--- api-comercio/src/main/java/br/com/empresa/api_comercio/repositories/UserRepository.java
+++ api-comercio/src/main/java/br/com/empresa/api_comercio/repositories/UserRepository.java
@@ -7,7 +7,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
 import br.com.empresa.api_comercio.entities.User;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID>{
 
 	List<User> findAllByFirstNameContainingIgnoreCase(@Param("firstName") String firstName);
