c716f05001ae187da3160688ca2e08d3abeda44f
IgorTecnologia <iguigu2019@gmail.com>
2024-12-19 06:05:25 -0300

    User Resource IT

--- api-comercio/src/test/java/br/com/empresa/api_comercio/resources/UserResourceIT.java
+++ api-comercio/src/test/java/br/com/empresa/api_comercio/resources/UserResourceIT.java
@@ -10,21 +10,19 @@ import br.com.empresa.api_comercio.services.impl.UserServiceImpl;
 import br.com.empresa.api_comercio.tests.*;
 import com.fasterxml.jackson.databind.*;
 import org.junit.jupiter.api.*;
-import org.mockito.*;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.boot.test.autoconfigure.web.servlet.*;
-import org.springframework.boot.test.mock.mockito.*;
-import org.springframework.data.domain.*;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.*;
 import org.springframework.test.web.servlet.*;
 
 import java.util.*;
 
-import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(UserResource.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class UserResourceIT {
 
     @Autowired
@@ -33,101 +31,78 @@ public class UserResourceIT {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @MockBean
+    @Autowired
     private UserServiceImpl service;
 
     @Autowired
     private UserRepository repository;
 
-    private UUID existingId;
-    private UUID nonExistingId;
-    private String firstName;
-
-    private PageImpl<UserDTO> page;
-
-    public UserResourceIT() throws Exception {
-    }
-
-    @BeforeEach
-    void setUp() throws Exception{
-
-        Optional<User> obj = repository.findAll().stream().findFirst();;
-        existingId = obj.orElseThrow(() -> new ResourceNotFoundException("Id not found: " + obj.get().getId())).getId();
-
-        nonExistingId = UUID.randomUUID();
-        firstName = "Maria";
-
-        UserDTO userDTO = Factory.createdUserDTO();
-
-        page = new PageImpl<>(List.of(userDTO));
-
-        when(service.findAllPaged(ArgumentMatchers.any())).thenReturn(page);
-
-        when(service.queryMethod(firstName)).thenReturn(page.stream().toList());
-
-        when(service.findById(existingId)).thenReturn(userDTO);
-        doThrow(ResourceNotFoundException.class).when(service).findById(nonExistingId);
-
-        when(service.insert(ArgumentMatchers.any())).thenReturn(userDTO);
-
-        when(service.update(ArgumentMatchers.eq(existingId), ArgumentMatchers.any())).thenReturn(userDTO);
-        when(service.update(ArgumentMatchers.eq(nonExistingId), ArgumentMatchers.any())).thenThrow(ResourceNotFoundException.class);
-
-        doNothing().when(service).deleteById(existingId);
-        doThrow(ResourceNotFoundException.class).when(service).deleteById(nonExistingId);
-    }
-
     @Test
     public void findAllPagedShouldReturnAllUsersSortByName() throws Exception {
 
         ResultActions result = mockMvc.perform(get("/users?sort=firstName"));
-                result.andExpect(status().isOk());
 
+                result.andExpect(status().isOk());
                 result.andExpect(jsonPath("$.content").exists());
                 result.andExpect(jsonPath("$.content[0].id").exists());
                 result.andExpect(jsonPath("$.content[0].firstName").exists());
                 result.andExpect(jsonPath("$.content[0].lastName").exists());
                 result.andExpect(jsonPath("$.content[0].email").exists());
-                result.andExpect(jsonPath("$.content[0].password").exists());
     }
 
     @Test
     public void queryMethodShouldReturnListFilteredByFirstName() throws Exception {
 
-        ResultActions result = mockMvc.perform(get("/users/firstName/{firstName}", firstName));
-                result.andExpect(status().isOk());
+        String firstName = "Gustavo";
 
+        ResultActions result = mockMvc.perform(get("/users/firstName/{firstName}", firstName));
+
+                result.andExpect(status().isOk());
                 result.andExpect(jsonPath("$[0].id").exists());
                 result.andExpect(jsonPath("$[0].firstName").value(firstName));
                 result.andExpect(jsonPath("$[0].lastName").exists());
                 result.andExpect(jsonPath("$[0].email").exists());
-                result.andExpect(jsonPath("$[0].password").exists());
     }
 
     @Test
-    public void findByIdShouldReturnObjectWhenIdExisting() throws Exception {
+    public void queryMethodShouldReturnStatusNotFoundWhenFirstNameNonExisting() throws Exception {
 
-        ResultActions result = mockMvc.perform(get("/users/{id}", existingId));
+        String firstName = "João";
+        ResultActions result = mockMvc.perform(get("/users/firstName/{firstName}", firstName));
+
+        result.andExpect(status().isNotFound());
+    }
+
+
+    @Test
+    public void findByIdShouldReturnUserByIdWhenIdExists() throws Exception {
+
+        Optional<User> obj = repository.findAll().stream().findFirst();;
+        UUID id = obj.orElseThrow(() -> new ResourceNotFoundException("Id not found: " + obj.get().getId())).getId();
+
+        ResultActions result = mockMvc.perform(get("/users/{id}", id));
                 result.andExpect(status().isOk());
 
                 result.andExpect(jsonPath("$.id").exists());
                 result.andExpect(jsonPath("$.firstName").exists());
                 result.andExpect(jsonPath("$.lastName").exists());
                 result.andExpect(jsonPath("$.email").exists());
-                result.andExpect(jsonPath("$.password").exists());
     }
 
     @Test
-    public void findByIdShouldThrowResourceNotFoundExceptionWhenIdNonExisting() throws Exception {
+    public void findByIdShouldReturnStatusNotFoundWhenIdNonExisting() throws Exception {
 
-        ResultActions result = mockMvc.perform(get("/users/{id}", nonExistingId));
+        UUID id = UUID.randomUUID();
+
+        ResultActions result = mockMvc.perform(get("/users/{id}", id)
+                .accept(MediaType.APPLICATION_JSON));
                 result.andExpect(status().isNotFound());
     }
 
     @Test
     public void insertShouldSaveObjectWhenCorrectStructure() throws Exception {
 
-        UserDTO dto = Factory.createdUserDTO();
+        UserDTO dto = Factory.createdUserDto();
 
         String jsonBody = objectMapper.writeValueAsString(dto);
 
@@ -135,61 +110,70 @@ public class UserResourceIT {
                 .content(jsonBody)
                     .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON));
-                            result.andExpect(status().isOk());
 
-                result.andExpect(jsonPath("$.id").exists());
-                result.andExpect(jsonPath("$.firstName").exists());
-                result.andExpect(jsonPath("$.lastName").exists());
-                result.andExpect(jsonPath("$.email").exists());
-                result.andExpect(jsonPath("$.password").exists());
-    }
-
-    @Test
-    public void updateShouldSaveObjectWhenIdExisting() throws Exception {
-
-        UserDTO dto = Factory.createdUserDTO();
-
-        String jsonBody = objectMapper.writeValueAsString(dto);
-
-        ResultActions result = mockMvc.perform(put("/users/{id}", existingId)
-                .content(jsonBody)
-                    .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON));
-                            result.andExpect(status().isOk());
-
-                result.andExpect(jsonPath("$.id").exists());
-                result.andExpect(jsonPath("$.firstName").exists());
-                result.andExpect(jsonPath("$.lastName").exists());
-                result.andExpect(jsonPath("$.email").exists());
-                result.andExpect(jsonPath("$.password").exists());
-    }
-
-    @Test
-    public void updateShouldThrowResourceNotFoundExceptionWhenIdNonExisting() throws Exception {
-
-        UserDTO dto = Factory.createdUserDTO();
-
-        String jsonBody = objectMapper.writeValueAsString(dto);
-
-        ResultActions result = mockMvc.perform(put("/users/{id}", nonExistingId)
-                .content(jsonBody)
-                    .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON));
-                            result.andExpect(status().isNotFound());
-
-    }
-
-    @Test
-    public void deleteByIdShouldDeleteObjectWhenIdExisting() throws Exception {
-
-        ResultActions result = mockMvc.perform(delete("/users/{id}", existingId));
-                result.andExpect(status().isNoContent());
-    }
-
-    @Test
-    public void deleteByIdShouldThrowResourceNotFoundExceptionWhenIdNonExisting() throws Exception {
-
-        ResultActions result = mockMvc.perform(delete("/users/{id}", nonExistingId));
                 result.andExpect(status().isOk());
+                result.andExpect(jsonPath("$.id").exists());
+                result.andExpect(jsonPath("$.firstName").exists());
+                result.andExpect(jsonPath("$.lastName").exists());
+                result.andExpect(jsonPath("$.email").exists());
+    }
+
+    @Test
+    public void updateShouldSaveObjectWhenIdExists() throws Exception {
+
+        Optional<User> obj = repository.findAll().stream().findFirst();;
+        UUID id = obj.orElseThrow(() -> new ResourceNotFoundException("Id not found: " + obj.get().getId())).getId();
+
+        UserDTO dto = Factory.createdUserDtoToUpdate();
+
+        String jsonBody = objectMapper.writeValueAsString(dto);
+
+        ResultActions result = mockMvc.perform(put("/users/{id}", id)
+                .content(jsonBody)
+                    .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON));
+
+                 result.andExpect(status().isOk());
+                result.andExpect(jsonPath("$.id").exists());
+                result.andExpect(jsonPath("$.firstName").exists());
+                result.andExpect(jsonPath("$.lastName").exists());
+                result.andExpect(jsonPath("$.email").exists());
+    }
+
+    @Test
+    public void updateShouldReturnStatusNotFoundWhenIdNonExists() throws Exception {
+
+        UUID id = UUID.randomUUID();
+
+        UserDTO dto = Factory.createdUserDtoToUpdateIsNotFound();
+
+        String jsonBody = objectMapper.writeValueAsString(dto);
+
+        ResultActions result = mockMvc.perform(put("/users/{id}", id)
+                .content(jsonBody)
+                    .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON));
+
+                result.andExpect(status().isNotFound());
+
+    }
+
+    @Test
+    public void deleteByIdShouldDeleteUserByIdWhenIdExists() throws Exception {
+
+        Optional<User> obj = repository.findAll().stream().findFirst();;
+        UUID id = obj.orElseThrow(() -> new ResourceNotFoundException("Id not found: " + obj.get().getId())).getId();
+
+        ResultActions result = mockMvc.perform(delete("/users/{id}", id));
+                result.andExpect(status().isOk());
+    }
+
+    @Test
+    public void deleteByIdShouldReturnStatusNotFoundWhenIdNonExisting() throws Exception {
+
+        UUID id = UUID.randomUUID();
+
+        ResultActions result = mockMvc.perform(delete("/users/{id}", id));
+                result.andExpect(status().isNotFound());
     }
 }
