26534f188497700f9dcc98fd229b70f00caf1330
IgorTecnologia <iguigu2019@gmail.com>
2024-12-19 06:03:55 -0300

    Role Service Tests

--- api-comercio/src/test/java/br/com/empresa/api_comercio/services/RoleServiceTests.java
+++ api-comercio/src/test/java/br/com/empresa/api_comercio/services/RoleServiceTests.java
@@ -1,7 +1,6 @@
 package br.com.empresa.api_comercio.services;
 
 import br.com.empresa.api_comercio.dto.*;
-import br.com.empresa.api_comercio.entities.Product;
 import br.com.empresa.api_comercio.entities.Role;
 import br.com.empresa.api_comercio.repositories.*;
 import br.com.empresa.api_comercio.services.exception.*;
@@ -10,6 +9,7 @@ import br.com.empresa.api_comercio.tests.*;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.boot.test.context.*;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.*;
 import org.springframework.transaction.annotation.*;
 
@@ -25,73 +25,118 @@ public class RoleServiceTests {
     @Autowired
     private RoleRepository repository;
 
-    private UUID existingId;
-    private UUID nonExistingId;
-    String authority;
-    private Long countTotalElements;
-
-    @BeforeEach
-    void setUp() throws Exception{
-
-        Optional<Role> obj = repository.findAll().stream().findFirst();;
-        existingId = obj.orElseThrow(() -> new ResourceNotFoundException("Id not found: " + obj.get().getId())).getId();
-
-        nonExistingId = UUID.randomUUID();
-        authority = "Manager";
-        countTotalElements = 3L;
-    }
-
     @Test
-    public void findAllPagedShouldReturnAllRoles(){
+    public void findAllPagedShouldReturnAllRolePaged(){
 
         PageRequest pageable = PageRequest.of(0, 12);
 
         Page<RoleDTO> page = service.findAllPaged(pageable);
 
+        Assertions.assertFalse(page.isEmpty());
         Assertions.assertNotNull(page);
     }
 
     @Test
-    public void findByIdShouldReturnObjectWhenIdExisting(){
+    public void findByIdShouldReturnRoleByIdWhenIdExists(){
 
-        RoleDTO dto = service.findById(existingId);
+        Optional<Role> obj = repository.findAll().stream().findFirst();;
+        UUID id = obj.orElseThrow(() -> new ResourceNotFoundException("Id not found: " + obj.get().getId())).getId();
+
+        RoleDTO dto = service.findById(id);
 
         Assertions.assertNotNull(dto);
     }
 
     @Test
+    public void findByIdShouldThrowResourceNotFoundExceptionWhenIdNonExisting(){
+
+        UUID id = UUID.randomUUID();
+
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+            RoleDTO dto = service.findById(id);
+            throw new ResourceNotFoundException("Id not found: " + id);
+        });
+    }
+
+    @Test
     public void queryMethodShouldReturnListFilteredByAuthority(){
+
+        String authority = "Manager";
 
         List<RoleDTO> list = service.queryMethod(authority);
 
+        Assertions.assertFalse(list.isEmpty());
         Assertions.assertNotNull(list);
+    }
+
+    @Test
+    public void queryMethodShouldThrowResourceNotFoundExceptionWhenAuthorityNonExisting(){
+
+        String authority = "Boss";
+
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+            List<RoleDTO> list = service.queryMethod(authority);
+            throw new ResourceNotFoundException("Authority not found: " + authority);
+        });
     }
 
     @Test
     public void insertShouldSaveObjectWhenCorrectStructure(){
 
-        RoleDTO dto = Factory.createdRoleDTO();
+        RoleDTO dto = Factory.createdRoleDto();
 
         service.insert(dto);
 
-        Assertions.assertEquals(countTotalElements +1, repository.count());
+        Assertions.assertNotNull(dto);
+        Assertions.assertEquals(4, repository.count());
     }
 
     @Test
     public void updateShouldSaveObjectWhenIdExisting(){
 
-        RoleDTO dto = Factory.createdRoleDTO();
+        Optional<Role> obj = repository.findAll().stream().findFirst();;
+        UUID id = obj.orElseThrow(() -> new ResourceNotFoundException("Id not found: " + obj.get().getId())).getId();
 
-        service.update(existingId, dto);
+        RoleDTO dto = Factory.createdRoleDtoToUpdate();
 
-        Assertions.assertEquals(countTotalElements, repository.count());
+        service.update(id, dto);
+
+        Assertions.assertNotNull(dto);
+        Assertions.assertEquals(3, repository.count());
+    }
+
+    @Test
+    public void updateShouldThrowResourceNotFoundExceptionWhenIdNonExisting(){
+
+        UUID id = UUID.randomUUID();
+
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+            RoleDTO dto = Factory.createdRoleDtoToUpdateIsNotFound();
+            dto = service.update(id, dto);
+            throw new ResourceNotFoundException("Id not found: " + id);
+        });
+    }
+
+    @Test
+    public void deleteByIdShouldThrowDataIntegrityViolationExceptionWhenIdIsAssociated(){
+
+        Optional<Role> obj = repository.findAll().stream().findFirst();;
+        UUID id = obj.orElseThrow(() -> new ResourceNotFoundException("Id not found: " + obj.get().getId())).getId();
+
+        Assertions.assertThrows(DataIntegrityViolationException.class, () -> {
+            service.deleteById(id);
+            throw new DataIntegrityViolationException("Data Integrity Violation Exception.");
+        });
     }
 
     @Test
     public void deleteByIdShouldThrowResourceNotFoundExceptionWhenIdNonExisting(){
 
+        UUID id = UUID.randomUUID();
+
         Assertions.assertThrows(ResourceNotFoundException.class, () -> {
-            service.deleteById(nonExistingId);
+            service.deleteById(id);
+            throw new ResourceNotFoundException("Id not found: " +id);
         });
     }
 }
