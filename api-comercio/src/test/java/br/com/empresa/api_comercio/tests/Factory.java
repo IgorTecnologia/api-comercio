b3cae72bc3a3b5bde94f80b7f72d61089a650db3
IgorTecnologia <iguigu2019@gmail.com>
2024-12-19 06:03:22 -0300

    Factory

--- api-comercio/src/test/java/br/com/empresa/api_comercio/tests/Factory.java
+++ api-comercio/src/test/java/br/com/empresa/api_comercio/tests/Factory.java
@@ -14,24 +14,52 @@ public class Factory {
         return entity;
     }
 
-    public static CategoryDTO createdCategoryDTO(){
+    public static CategoryDTO createdCategoryDto(){
 
         UUID id = UUID.randomUUID();
-        CategoryDTO dto = new CategoryDTO(id,"Frios");
+        CategoryDTO dto = new CategoryDTO(id,"Lanches");
+        return dto;
+    }
+
+    public static CategoryDTO createdCategoryDtoToUpdate(){
+
+        UUID id = UUID.randomUUID();
+        CategoryDTO dto = new CategoryDTO(id,"Bebibas");
+        return dto;
+    }
+
+    public static CategoryDTO createdCategoryDtoToUpdateIsNotFound(){
+
+        UUID id = UUID.randomUUID();
+        CategoryDTO dto = new CategoryDTO(id,"Limpeza");
         return dto;
     }
 
     public static Product createdProduct(){
 
         UUID id = UUID.randomUUID();
-        Product entity = new Product(id,"Leite", 6.5, "Leite integral", "www.img.com");
+        Product entity = new Product(id,"Leite", 7.77, "Leite integral", "www.img.com");
         return entity;
     }
 
-    public static ProductDTO createdProductDTO(){
+    public static ProductDTO createdProductDto(){
 
         UUID id = UUID.randomUUID();
-        ProductDTO dto = new ProductDTO(id,"Leite", 6.5, "Leite integral", "www.img.com");
+        ProductDTO dto = new ProductDTO(id,"Azeite", 17.77, "De olívia", "www.img.com");
+        return dto;
+    }
+
+    public static ProductDTO createdProductDtoToUpdate(){
+
+        UUID id = UUID.randomUUID();
+        ProductDTO dto = new ProductDTO(id,"Manteiga", 13.99, "Com sal", "www.img.com");
+        return dto;
+    }
+
+    public static ProductDTO createdProductDtoUpdateIsNotFound(){
+
+        UUID id = UUID.randomUUID();
+        ProductDTO dto = new ProductDTO(id,"Biscoito", 3.77, "Chocolate", "www.img.com");
         return dto;
     }
 
@@ -42,12 +70,25 @@ public class Factory {
         return entity;
     }
 
-    public static RoleDTO createdRoleDTO(){
+    public static RoleDTO createdRoleDto(){
 
-        UUID id = UUID.randomUUID();
-        RoleDTO dto = new RoleDTO(id,"Manager");
+        RoleDTO dto = new RoleDTO(null,"Coordinator");
         return dto;
     }
+
+    public static RoleDTO createdRoleDtoToUpdate(){
+
+        RoleDTO dto = new RoleDTO(null, "Assistant");
+        return dto;
+    }
+
+    public static RoleDTO createdRoleDtoToUpdateIsNotFound(){
+
+        UUID id = UUID.randomUUID();
+        RoleDTO dto = new RoleDTO(id, "CEO");
+        return dto;
+    }
+
     public static User createdUser(){
 
         UUID id = UUID.randomUUID();
@@ -55,10 +96,25 @@ public class Factory {
         return entity;
     }
 
-    public static UserDTO createdUserDTO(){
+    public static UserDTO createdUserDto(){
 
         UUID id = UUID.randomUUID();
         UserDTO dto = new UserDTO(id, "Bruno", "Abulquerque", "bruno@gmail.com", "123456");
         return dto;
     }
+
+    public static UserDTO createdUserDtoToUpdate(){
+
+        UUID id = UUID.randomUUID();
+        UserDTO dto = new UserDTO(id, "Bruna", "Ferraz", "bruna@gmail.com", "123456");
+        return dto;
+    }
+
+    public static UserDTO createdUserDtoToUpdateIsNotFound(){
+
+        UUID id = UUID.randomUUID();
+        UserDTO dto = new UserDTO(id, "Marcos", "Gonçalves", "marcos@gmail.com", "123456");
+        return dto;
+    }
+
 }
